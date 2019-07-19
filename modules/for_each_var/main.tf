80d3e6bd8d660abdfb0f01b53f3b93609827d973
Bent Terp <bent.terp@folksam.se>
2019-07-19 09:02:39 +0200

    Added the testcase provided by tmccombs

--- modules/for_each_var/main.tf
+++ modules/for_each_var/main.tf
@@ -1,5 +1,9 @@
+locals {
+  mylocalmap = var.mymap
+}
+
 resource "azurerm_resource_group" "myrg" {
-  for_each = var.mymap
+  for_each = local.mylocalmap
   name     = each.key
   location = "westeurope"
   tags = merge({"Description" = each.value})
